47decf3d0c9b5ca35b3a00d536759d5a792c05e0
Artem Kruglyakov <kruglakovartem@gmail.com>
2023-01-26 23:56:36 +0300

    small daily update

--- api_expenser/API/Expenser_Api.h
+++ api_expenser/API/Expenser_Api.h
@@ -1,6 +1,6 @@
 #include <vector>
 #include <list> 
-
+#include "expenser_impl.h"
 namespace Expenser
 {
 class Expenser_Api
@@ -9,14 +9,15 @@ public:
     Expenser_Api() = delete;
 
     // Expense_Api* create_expense_api(db_file ..., error&)
-    //std::list<Expenser::Users> get_users();
-    //std::list<Expenser::Expense> get_expense(dates ...);
-    //std::list<Expenser::Expense> get_expense(filters ...);
-    //std::list<Expenser::Revenue> get_revenue(dates ...);
-    //std::list<Expenser::Revenue> get_revenue(filters ...);
-    //std::list<Expenser::Debt> get_debt(dates ...);
-    //std::list<Expenser::Debt> get_debt(filters ...);
-    // bool add_expense(Expense ...)
+    //std::list<Expenser::Users> get_users() const;
+    //std::list<Expenser::Expense> get_expense(dates ...) const;
+    //std::list<Expenser::Expense> get_expense(filters ...) const;
+    //std::list<Expenser::Revenue> get_revenue(dates ...) const;
+    //std::list<Expenser::Revenue> get_revenue(filters ...) const;
+    //std::list<Expenser::Debt> get_debt(dates ...) const;
+    //std::list<Expenser::Debt> get_debt(filters ...) const;
+    // std::list<Category> get_category() const;
+    // bool add_expense(Expense ...) 
     // bool add_expense(std::list<Expense> ...)
     // bool add_revenue(Revenue ...)
     // bool add_revenue(std::list<Revenue> ...)
@@ -25,11 +26,18 @@ public:
     // bool add_user(User ...)
     // bool add_category(Category ...)
 
+    // async methods ???
+    // bool add_expense(Expense ..., callback function ???)
+
     
 private:
     // create_expense_api -> singleton or factory 
     // get_users() -> invoke Expense_Impl
     // ...
+
+    // std::shared_ptr<Expenser_Impl> expenser_impl{ nullptr }};
+    // std::shared_ptr<sqlite3_db> db;
+
 
 };
 
